f9d09b8401d6f28a0ead23fd0bb5e2b16ae45033
Shubh <shubhamghardale10@gmail.com>
2025-09-09 12:51:55 +0530

    07 Playground Functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/providers/theme-providers";
+
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
+import { ThemeProvider } from "@/components/providers/theme-providers";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,25 +24,34 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
-  const session = await auth();
+}>) {
+
+  const session = await auth()
 
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <SessionProvider session={session}>
-          <ThemeProvider
+    <SessionProvider session={session}>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+         <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
-            {children}
-          </ThemeProvider>
-        </SessionProvider>
+            <div className="flex flex-col min-h-screen">
+              <Toaster/>
+    <div className="flex-1">
+{children}
+    </div>
+            </div>
+        
+        </ThemeProvider>
       </body>
     </html>
+    </SessionProvider>
   );
 }
